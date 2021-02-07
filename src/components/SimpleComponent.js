c2328fd1a844a0bfa9d479e737097ac7e1830043
noelanimanini <noelani26@crystals-MacBook-Pro.local>
2021-02-06 23:51:28 -0800

    its the STATE of mind

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponent Here
+import React from 'react'; 
+
+export default class SimpleComponent extends React.Component {
+
+    state = {
+        mood: "happy"
+    }
+
+    handleClick = (event) => {
+        // event.preventDefault() 
+        this.setState({
+            mood: "sad"
+        })
+    }
+        render () {
+            return (
+                <div onClick={this.handleClick}>
+                    {this.state.mood}
+                </div>
+            )
+        }
+}
